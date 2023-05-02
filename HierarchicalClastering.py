f0a088d4d53605e5599d398678726f8a6d7dc9c4
OllaberganYuldashov <ollaberganyuldashov@gmail.com>
2023-05-02 12:45:44 +0500

    25.04.2023

--- HierarchicalClastering.py
+++ HierarchicalClastering.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt1
 import numpy as np
 
-X = np.array([[2,2],[3,5],[7,9],[6,15],[3,7],[7,5],[3,8]])
-labels = range(1, 8)
+X = np.array([[9,15],[6,12],[11,9],[10,4],[7,10],[12,4],[2,3],[1,1],[8,4],[10,1]])
+labels = range(1, len(X)+1)
 plt1.figure(figsize=(10, 7))
 plt1.subplots_adjust(bottom=0.1)
 plt1.scatter(X[:,0],X[:,1], label='True Position')
@@ -14,8 +14,8 @@ plt1.show()
 from scipy.cluster.hierarchy import dendrogram, linkage
 from matplotlib import pyplot as plt2
 linked = linkage(X, 'single')
-labelList = range(1, 8)
-plt2.figure(figsize=(10, 7))
+labelList = range(1, len(X)+1)
+plt2.figure(figsize=(10, 6))
 
 dendrogram(linked, orientation='top',labels=labelList,
 distance_sort='descending',show_leaf_counts=True)
